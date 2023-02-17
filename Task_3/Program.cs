cc859ba0f2096bc52229339071f0049bf270dc7b
nina <n.yudkina95@yandex.ru>
2023-02-17 17:08:49 +0300

    Added Daysof week

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿Console.WriteLine("Введите номер дня недели");
+int number =Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) 
+{
+    Console.WriteLine("Monday");
+}
+    else 
+    if (number == 2)
+    {
+        Console.WriteLine("Tuesday");
+    } 
+else 
+    if (number == 3)
+    {
+        Console.WriteLine("Wednesday");
+    } 
+else 
+    if (number == 4)
+    {
+        Console.WriteLine("Thuesday");
+    } 
+else 
+    if (number == 5)
+    {
+        Console.WriteLine("Friday");
+    } 
+else 
+    if (number == 6)
+    {
+        Console.WriteLine("Saturday");
+    } 
+else 
+    if (number == 7)
+    {
+        Console.WriteLine("Sunday");
+    } 
+
+    
