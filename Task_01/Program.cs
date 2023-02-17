fc05626f0a3daaef8a0e8f3b2343e1e8de48f649
nina <n.yudkina95@yandex.ru>
2023-02-17 20:28:15 +0300

    Added Square of number

---
+++ Task_01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите квадрат числа");
+int square =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int number =Convert.ToInt32(Console.ReadLine());
+if (square == number*number) 
+{
+    Console.WriteLine("True");
+}
+    else 
+    Console.WriteLine("False");
