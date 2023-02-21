9edddbbf4aaf168e18fb6e5f6cd820fcd634a19a
nina <n.yudkina95@yandex.ru>
2023-02-22 00:12:24 +0300

    Added Task_10

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -17,11 +17,11 @@ Console.WriteLine($"Случайное число из отрезка 10-99 => {
 int maxDigit = MaxDigit(numberR);
 Console.WriteLine($"Наибольшая цифра числа-> {maxDigit}");
 
-int MaxDigit(int num)
+int MaxDigit(int num) // создается копия number. number=num
 {
     int firstDigit = num / 10;
     int secondDigit = num % 10;
-    
+
     // if (firstDigit > secondDigit) return firstDigit;
     // return secondDigit;   //else можно не использовать после return
 
