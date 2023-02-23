17abc19ae95536fa46a7256196b0e0355cc2b035
nina <n.yudkina95@yandex.ru>
2023-02-23 13:30:52 +0300

    Added Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if  (number>99&&number<=999)
+{
+    int secDigit = SecDigit(number);
+Console.WriteLine($"Второе число-> {secDigit}");
+
+int SecDigit(int num) // создается копия numberR. number=num
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int secondDigit = (num-(firstDigit*100+thirdDigit))/10;
+
+ 
+    return secondDigit;
+}
+}
+else
+Console.WriteLine("Дурачок!");
