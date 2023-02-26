6b74aff16a84b67516b0e449a94b40610a58a72f
nina <n.yudkina95@yandex.ru>
2023-02-27 00:04:02 +0300

    Added Task_29_DZ

---
+++ Task_29_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int [] array=new int [8]{1, 2, 5, 7, 19, 6, 1, 33};
+
+
+PrintArray(array);
