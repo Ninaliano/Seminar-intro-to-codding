d79872aa00bbd5ed5d049fceeeba7b765ca802e6
nina <n.yudkina95@yandex.ru>
2023-03-03 00:46:03 +0300

    Added Task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+int[] FillArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = FillArrayRndInt(12, -10, 10);
+PrintArray(array);
+
+int[] Reverce(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i] * (-1);
+    }
+    return result;
+}
+
+
+
+Console.Write($"Перевернутая матрица: ");
+int[] reverce = Reverce(array);
+PrintArray(reverce);
+
