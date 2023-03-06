bf766c80a59f4057a3ee20d67e8dbe45fe5299ab
nina <n.yudkina95@yandex.ru>
2023-03-06 23:49:21 +0300

    Added Task_51

---
+++ Task_51/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];   // 0, 1
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)   //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // columns
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElemsMainDiag(int[,] matrixx)
+{
+    int sumElemsMainDiag = new int();
+    for (int i = 0; i < matrixx.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixx.GetLength(1); j++)
+        {
+            if (i == j) sumElemsMainDiag = matrixx[i, j] + sumElemsMainDiag;
+        }
+    }
+    return sumElemsMainDiag;
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(array2d);
+int sumElemsMainDiag = SumElemsMainDiag(array2d);
+Console.WriteLine($"Сумма элементов главной диагонали:  {sumElemsMainDiag} ");
+
