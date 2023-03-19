ed6d4f243bdad14aaaaf241c63cb190064e2a8db
nina <n.yudkina95@yandex.ru>
2023-03-20 01:04:50 +0300

    Added Task_50_DZ

---
+++ Task_50_DZ/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];   // 0, 1
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)   //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // columns
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(" | ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine(" | ");
+    }
+}
+
+void GetMatrix(int[,] matrix, int i, int j)
+{
+     if (i<matrix.GetLength(0) && j<matrix.GetLength(1) && i>=0 && j>=0)  
+    Console.WriteLine (matrix[i,j]);
+    else Console.WriteLine ($"Такого элемента нет");
+}
+
+int Promt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
+int position1 =Promt("Введите номер ряда ->   ");
+int position2 =Promt("Введите номер колонки ->   ");
+GetMatrix(array2d, position1, position2);
