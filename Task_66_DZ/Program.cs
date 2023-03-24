94eb5989810fada9cfc715ac00e7e46b5687794b
nina <n.yudkina95@yandex.ru>
2023-03-25 00:15:56 +0300

    Added Task_66_DZ

---
+++ Task_66_DZ/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Promt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int M, int N)
+{
+    if (N >= M)
+    {
+        return M + Sum(M + 1, N);
+    }
+    return 0;
+}
+
+int m = Promt("Задайте значение M:");
+int n = Promt("Задайте значение N:");
+int sum = 0;
+if (n > m)
+    sum = Sum(m, n);
+else
+    sum = Sum(n, m);
+
+Console.WriteLine($"M={m}, N={n}, --> {sum}");
+
+
