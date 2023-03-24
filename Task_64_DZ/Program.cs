db7f1e4b84165d9315064d637f337cf52999fdde
nina <n.yudkina95@yandex.ru>
2023-03-25 00:15:46 +0300

    Added Task_64_DZ

---
+++ Task_64_DZ/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Promt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumber(int R)
+{
+    if (R > 0)
+    {
+        Console.Write($"{R}");
+        if (R > 1)
+        {
+            Console.Write(", ");
+        }
+        PrintNumber(R - 1);
+    }
+}
+
+
+int n = Promt("Задайте значение:");
+PrintNumber(n);
+
+
+
+
+
+
+
